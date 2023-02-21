22a18b1f8163bb675671db4264e64ba111a66f34
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-21 11:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ const slides = document.querySelectorAll('.slider-container');
+      let currentSlide = 0;
+      const nextSlide = () => {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+      };
+      const prevSlide = () => {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        slides[currentSlide].classList.add('active');
+      };
+      document.querySelector('.down-button').addEventListener('click', nextSlide);
+      document.querySelector('.up-button').addEventListener('click', prevSlide);
